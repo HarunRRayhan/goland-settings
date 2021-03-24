03c2aecafa93949e5ff675cfa5f6dc94f19eefe8
Harun R Rayhan <harun.b13@gmail.com>
2021-03-24 13:42:25 +0600

    PS-2020.3.3 <rayhan@rays-mbp16.local Create debugger.xml, PHP Class.php, updates.xml, keymap.xml, Default copy.xml, rainbow_brackets.xml, editor.xml, Default.xml, ide.general.xml, material_theme.xml, project.default.xml, _@user_Monokai.icls, code.style.schemes.xml, ea_extended.xml, _@user_Material Oceanic.icls, databaseSettings.xml, pluginAdvertiser.xml, _@user_Darcula.icls, web-browsers.xml, usageView.xml, earthsong.icls, remote-servers.xml, Default.xml, PHP.xml, javaeeExternalResources.xml, log_highlighting.xml, lightEdit.xml, notifications.xml, filetypes.xml, terminal.xml, PHP File.php, Bootstrap 4.xml, colors.scheme.xml, databaseDrivers.xml, _@user_Default.icls, material_custom_theme.xml, user.xml, Rayhan.icls, HRXLabs Themes.php, laf.xml, webServers.xml, WordPress.xml, IntelliLang.xml, baseRefactoring.xml, Eloquent Model.php, intentionSettings.xml, markdown.xml, watcherDefaultTasks.xml, ui.lnf.xml, web-types-registry.xml, customization.xml, SdkSettingsPlugin.xml, rayhan_s keymap.xml, path.macros.xml, bootstrap.xml, _@user_Material Palenight.icls, PHP File Header.php, Default _1_.xml, applicationLibraries.xml, php.xml, find.xml, diff.xml, parameter.hints.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,10 @@
+/**
+ * @author HarunRRayhan/HRXPlugins
+ * @url http://hrxplugins.com/
+ * @version 1.0
+ * File: ${FILE_NAME}
+ * @FileVersion: 1.0
+ * Created On: ${DATE}:${TIME} ${MONTH}/${DAY}/${YEAR}
+ * Updated On: ${DATE}:${TIME} ${MONTH}/${DAY}/${YEAR}
+ * @package: @awesome-logo-slider-pro
+ */
